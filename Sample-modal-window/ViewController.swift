f547cea9552f19e422992302adb2d0be1d935dfa
takumi314 <isospin_symmetry@me.com>
2018-12-30 03:15:42 +0900

    Initial commit

---
+++ Sample-modal-window/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample-model-window
+//
+//  Created by NishiokaKohei on 30/12/2018.
+//  Copyright © 2018 Takumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
