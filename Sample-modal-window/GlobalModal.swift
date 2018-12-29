7c03627165e74f9abc527e6063c8c04af462fd62
takumi314 <isospin_symmetry@me.com>
2018-12-30 03:43:29 +0900

    [WIP] make GlobalModal

---
+++ Sample-modal-window/GlobalModal.swift
@@ -0,0 +1,44 @@
+//
+//  GlobalModal.swift
+//  Sample-modal-window
+//
+//  Created by NishiokaKohei on 30/12/2018.
+//  Copyright © 2018 Takumi. All rights reserved.
+//
+
+import UIKit
+
+///
+/// グローバルにモーダルを表示／非表示するクラス
+///
+class GlobalModal {
+
+    static let shared = GlobalModal()
+
+    private let window: UIWindow = {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.backgroundColor = .clear
+        return window
+    }()
+
+    func present(_ viewController: UIViewController, animated: Bool, completion: (() -> Void)?) {
+        dismiss(animated: animated, completion: nil)
+        let rootViewController = UIViewController()
+        rootViewController.view.backgroundColor = .clear
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
+        rootViewController.present(viewController, animated: animated, completion: completion)
+    }
+
+    func dismiss(animated: Bool, completion: (() -> Void)?) {
+        let completion: () -> Void = {
+            completion?()
+            self.window.isHidden = true
+        }
+        window.rootViewController?.presentedViewController?.dismiss(
+            animated: animated,
+            completion: completion
+        )
+    }
+
+}
